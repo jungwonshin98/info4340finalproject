1061ad4357d0b7638589bc9d741bb320360c2aed
Jungwon Shin <Jungwon.shin98@gmail.com>
2020-11-19 16:46:30 -0500

    dist

---
+++ dist/precache-manifest.9c8d3a4c16154ca66b75a4ad6aec7a69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f23d908df7173fce4c4",
+    "url": "/css/app.d97f1df2.css"
+  },
+  {
+    "revision": "8f23d908df7173fce4c4",
+    "url": "/js/app.5a9096f7.js"
+  },
+  {
+    "revision": "a994424c4079f84b34df",
+    "url": "/css/chunk-vendors.a08731a7.css"
+  },
+  {
+    "revision": "a994424c4079f84b34df",
+    "url": "/js/chunk-vendors.339c959b.js"
+  },
+  {
+    "revision": "1c05e62d8b25464048bdff67110868f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
